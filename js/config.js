113516d56ade6a222b8ed9d8605487ed1e2aad30
sandeli <sandeli79@gmail.com>
2025-08-10 09:42:59 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    name: 'TGTV',
+    url: 'https://lys.yyds8.eu.org',
+    description: 'TG免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
